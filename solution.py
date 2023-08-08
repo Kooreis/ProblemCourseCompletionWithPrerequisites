55ea16733dcfdb9606fda508b8ff980bf7a5b209
Kooreis <kooreis.official@gmail.com>
2023-08-07 20:34:55 -0500

    In this update, a Depth-First Search (DFS) function is defined within the 'canFinish' function. This function is used to detect if there is a cycle in the graph. If a cycle is detected, it returns False. If no cycle is detected, it returns True.

--- solution.py
+++ solution.py
@@ -1,6 +1,12 @@
-from collections import defaultdict
-
-def canFinish(numCourses, prerequisites):
-    graph = defaultdict(list)
-    for x, y in prerequisites:
-        graph[x].append(y)
+visited = [0 for _ in range(numCourses)]
+    def dfs(i):
+        if visited[i] == -1:
+            return False
+        if visited[i] == 1:
+            return True
+        visited[i] = -1
+        for j in graph[i]:
+            if not dfs(j):
+                return False
+        visited[i] = 1
+        return True
