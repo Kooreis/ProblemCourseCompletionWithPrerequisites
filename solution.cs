ac531342905f9cfdd00de4b2c62587c75879af8d
Kooreis <kooreis.official@gmail.com>
2023-08-07 20:34:55 -0500

    In the second update, the CanFinish function is called with the number of courses and their prerequisites as arguments. The result is then printed to the console.

--- solution.cs
+++ solution.cs
@@ -1,15 +1,2 @@
-using System;
-using System.Collections.Generic;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        int numCourses = 4;
-        int[][] prerequisites = new int[][] {
-            new int[] {1, 0},
-            new int[] {2, 1},
-            new int[] {3, 2},
-            new int[] {1, 3}
-        };
-    }
+bool canFinish = CanFinish(numCourses, prerequisites);
+        Console.WriteLine(canFinish);
